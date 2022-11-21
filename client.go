c3ab3ebde595435ae95554897903c832996a0024
Graza <code@graza.io>
2022-11-21 08:26:26 +0000

    chore: manual go fmt

--- client.go
+++ client.go
@@ -1,201 +1,201 @@
 package uptimerobotapi
 
 import (
-    "encoding/json"
-    "fmt"
-    "github.com/google/go-querystring/query"
-    "io/ioutil"
-    "net/http"
-    "net/url"
-    "strings"
+	"encoding/json"
+	"fmt"
+	"github.com/google/go-querystring/query"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
 
 const (
 
-    // defaultBaseURL is the base URL of the UptimeRobot API.
-    defaultBaseURL = "https://api.uptimerobot.com/"
+	// defaultBaseURL is the base URL of the UptimeRobot API.
+	defaultBaseURL = "https://api.uptimerobot.com/"
 
-    // apiVersion is the revision of the UptimeRobot API.
-    apiVersion = "v2"
+	// apiVersion is the revision of the UptimeRobot API.
+	apiVersion = "v2"
 
-    // apiVersion default user agent of the  UptimeRobot API.
-    defaultUserAgent = "go-uptimerobotapi"
+	// apiVersion default user agent of the  UptimeRobot API.
+	defaultUserAgent = "go-uptimerobotapi"
 )
 
 // ClientConfig specifies configuration with which to initialize a UptimeRobot API
 // client.
 type ClientConfig struct {
-    // APIToken is the UptimeRobot API token to use for authentication.
-    APIToken string
+	// APIToken is the UptimeRobot API token to use for authentication.
+	APIToken string
 
-    // HTTPClient is your own HTTP client. The library will otherwise use a
-    // parameter-less `&http.Client{}`, resulting in default everything.
-    HTTPClient *http.Client
+	// HTTPClient is your own HTTP client. The library will otherwise use a
+	// parameter-less `&http.Client{}`, resulting in default everything.
+	HTTPClient *http.Client
 
-    // UserAgent User agent used when communicating with the UptimeRobot API.
-    UserAgent *string
+	// UserAgent User agent used when communicating with the UptimeRobot API.
+	UserAgent *string
 }
 
 // A Client manages communication with the UptimeRobot API.
 type Client struct {
 
-    // HTTP client used to communicate with the API.
-    httpClient *http.Client
-    // Base URL for API requests.
-    baseUrl *url.URL
+	// HTTP client used to communicate with the API.
+	httpClient *http.Client
+	// Base URL for API requests.
+	baseUrl *url.URL
 
-    apiVersion string
-    // User agent used when communicating with the Uptime Robot API.
-    userAgent string
+	apiVersion string
+	// User agent used when communicating with the Uptime Robot API.
+	userAgent string
 
-    // Token used to make authenticated API calls.
-    Token string
+	// Token used to make authenticated API calls.
+	Token string
 
-    // Services used for talking to different parts of the UptimeRobot API.
-    Account      AccountService
-    AlertContact AlertContactService
-    Monitor      MonitorService
-    MWindow      MWindowService
-    PSP          PSPService
+	// Services used for talking to different parts of the UptimeRobot API.
+	Account      AccountService
+	AlertContact AlertContactService
+	Monitor      MonitorService
+	MWindow      MWindowService
+	PSP          PSPService
 }
 
 // NewClient returns a new UptimeRobot API client.
 func NewClient(token string) *Client {
-    client := newClient(nil)
+	client := newClient(nil)
 
-    client.Token = token
-    return client
+	client.Token = token
+	return client
 }
 
 // NewClientWithConfig returns a new UptimeRobot API client.
 func NewClientWithConfig(config *ClientConfig) *Client {
-    client := newClient(config)
-    return client
+	client := newClient(config)
+	return client
 }
 
 // newClient returns a new UptimeRobot API client.
 func newClient(config *ClientConfig) *Client {
 
-    if config == nil {
-        config = &ClientConfig{}
-    }
+	if config == nil {
+		config = &ClientConfig{}
+	}
 
-    var httpClient *http.Client
+	var httpClient *http.Client
 
-    if config.HTTPClient == nil {
-        httpClient = &http.Client{}
-    } else {
-        httpClient = config.HTTPClient
-    }
+	if config.HTTPClient == nil {
+		httpClient = &http.Client{}
+	} else {
+		httpClient = config.HTTPClient
+	}
 
-    var userAgent string
+	var userAgent string
 
-    if config.UserAgent == nil {
-        userAgent = defaultUserAgent
-    } else {
-        userAgent = *config.UserAgent
-    }
+	if config.UserAgent == nil {
+		userAgent = defaultUserAgent
+	} else {
+		userAgent = *config.UserAgent
+	}
 
-    apiUrl, err := url.Parse(defaultBaseURL)
-    if err != nil {
-        panic(err)
-    }
+	apiUrl, err := url.Parse(defaultBaseURL)
+	if err != nil {
+		panic(err)
+	}
 
-    c := &Client{
-        httpClient: httpClient,
-        baseUrl:    apiUrl,
-        apiVersion: apiVersion,
-        Token:      config.APIToken,
-        userAgent:  userAgent,
-    }
+	c := &Client{
+		httpClient: httpClient,
+		baseUrl:    apiUrl,
+		apiVersion: apiVersion,
+		Token:      config.APIToken,
+		userAgent:  userAgent,
+	}
 
-    c.Account = AccountService{apiClient: c}
-    c.AlertContact = AlertContactService{apiClient: c}
-    c.Monitor = MonitorService{apiClient: c}
-    c.MWindow = MWindowService{apiClient: c}
-    c.PSP = PSPService{apiClient: c}
-    return c
+	c.Account = AccountService{apiClient: c}
+	c.AlertContact = AlertContactService{apiClient: c}
+	c.Monitor = MonitorService{apiClient: c}
+	c.MWindow = MWindowService{apiClient: c}
+	c.PSP = PSPService{apiClient: c}
+	return c
 }
 
 func (c *Client) request(method string, urlStr string, opt interface{}, responseModel interface{}) error {
-    u, err := c.baseUrl.Parse(fmt.Sprintf("%s/%s", c.apiVersion, urlStr))
-    if err != nil {
-        return err
-    }
+	u, err := c.baseUrl.Parse(fmt.Sprintf("%s/%s", c.apiVersion, urlStr))
+	if err != nil {
+		return err
+	}
 
-    q, err := query.Values(opt)
-    if err != nil {
-        return err
-    }
+	q, err := query.Values(opt)
+	if err != nil {
+		return err
+	}
 
-    q.Set("api_key", c.Token)
-    q.Set("format", "json")
+	q.Set("api_key", c.Token)
+	q.Set("format", "json")
 
-    req, err := http.NewRequest(method, u.String(), strings.NewReader(q.Encode()))
-    if err != nil {
-        return err
-    }
+	req, err := http.NewRequest(method, u.String(), strings.NewReader(q.Encode()))
+	if err != nil {
+		return err
+	}
 
-    req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-    res, err := c.httpClient.Do(req)
-    if err != nil {
-        return err
-    }
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
 
-    defer res.Body.Close()
+	defer res.Body.Close()
 
-    if res.StatusCode != http.StatusOK {
-        return APIError{
-            StatusCode: res.StatusCode,
-            Message:    fmt.Sprintf("HTTP response with status code %d", res.StatusCode),
-        }
-    }
+	if res.StatusCode != http.StatusOK {
+		return APIError{
+			StatusCode: res.StatusCode,
+			Message:    fmt.Sprintf("HTTP response with status code %d", res.StatusCode),
+		}
+	}
 
-    var apiResp map[string]interface{}
+	var apiResp map[string]interface{}
 
-    bodyByte, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        return err
-    }
+	bodyByte, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
 
-    err = c.decodeAPIResponse(bodyByte, &apiResp)
-    if err != nil {
-        return err
-    }
+	err = c.decodeAPIResponse(bodyByte, &apiResp)
+	if err != nil {
+		return err
+	}
 
-    if apiResp["stat"] != StatOk {
-        var apiErrResp ErrorResponse
+	if apiResp["stat"] != StatOk {
+		var apiErrResp ErrorResponse
 
-        err := createFromMap(apiResp, &apiErrResp)
+		err := createFromMap(apiResp, &apiErrResp)
 
-        if err != nil {
-            return err
-        }
+		if err != nil {
+			return err
+		}
 
-        apiErrResp.Error.StatusCode = res.StatusCode
+		apiErrResp.Error.StatusCode = res.StatusCode
 
-        return &apiErrResp.Error
-    }
+		return &apiErrResp.Error
+	}
 
-    err = createFromMap(apiResp, &responseModel)
+	err = createFromMap(apiResp, &responseModel)
 
-    return err
+	return err
 }
 
 // decodeAPIResponse decode response
 func (c *Client) decodeAPIResponse(responseBody []byte, resp interface{}) (err error) {
-    err = json.Unmarshal(responseBody, resp)
-    if err != nil {
-        return
-    }
+	err = json.Unmarshal(responseBody, resp)
+	if err != nil {
+		return
+	}
 
-    return nil
+	return nil
 }
 
 // createFromMap create json from map
 func createFromMap(m map[string]interface{}, result interface{}) error {
-    data, _ := json.Marshal(m)
-    err := json.Unmarshal(data, &result)
-    return err
+	data, _ := json.Marshal(m)
+	err := json.Unmarshal(data, &result)
+	return err
 }
--- psp.go
+++ psp.go
@@ -3,86 +3,86 @@ package uptimerobotapi
 import "net/http"
 
 type PSPService struct {
-    apiClient *Client
+	apiClient *Client
 }
 
 type GetPSPsParams struct {
-    // PSPs should be provided as IDs separated with hyphens '-' example: 123-4567-845
-    PSPs   string `url:"psps,omitempty"`
-    Offset int    `url:"offset,omitempty"`
-    Limit  *int   `url:"limit,omitempty"`
+	// PSPs should be provided as IDs separated with hyphens '-' example: 123-4567-845
+	PSPs   string `url:"psps,omitempty"`
+	Offset int    `url:"offset,omitempty"`
+	Limit  *int   `url:"limit,omitempty"`
 }
 
 type PSPsResp struct {
-    Stat       string     `json:"stat"`
-    Pagination Pagination `json:"pagination"`
-    PSPs       []PSP      `json:"psps"`
+	Stat       string     `json:"stat"`
+	Pagination Pagination `json:"pagination"`
+	PSPs       []PSP      `json:"psps"`
 }
 
 type PSP struct {
-    ID           int    `json:"id"`
-    FriendlyName string `json:"friendly_name"`
-    Monitors     int    `json:"monitors"`
-    Sort         int    `json:"sort"`
-    Status       int    `json:"status"`
-    StandardURL  string `json:"standard_url"`
-    CustomURL    string `json:"customURL"`
+	ID           int    `json:"id"`
+	FriendlyName string `json:"friendly_name"`
+	Monitors     int    `json:"monitors"`
+	Sort         int    `json:"sort"`
+	Status       int    `json:"status"`
+	StandardURL  string `json:"standard_url"`
+	CustomURL    string `json:"customURL"`
 }
 
 type NewPSPParams struct {
-    Type         int    `url:"type"`
-    FriendlyName string `url:"friendly_name"`
-    // Monitors should be provided as hyphen separated IDs (or 0 for all)
-    Monitors     string `url:"monitors"`
-    CustomDomain string `url:"custom_domain,omitempty"`
-    Password     string `url:"password,omitempty"`
-    Sort         int    `url:"sort,omitempty"`
-    HideURLLinks bool   `url:"hide_url_links,omitempty"`
-    // Status should be 0 (paused) or 1 (active), other values may return an error
-    Status int `url:"status,omitempty"`
+	Type         int    `url:"type"`
+	FriendlyName string `url:"friendly_name"`
+	// Monitors should be provided as hyphen separated IDs (or 0 for all)
+	Monitors     string `url:"monitors"`
+	CustomDomain string `url:"custom_domain,omitempty"`
+	Password     string `url:"password,omitempty"`
+	Sort         int    `url:"sort,omitempty"`
+	HideURLLinks bool   `url:"hide_url_links,omitempty"`
+	// Status should be 0 (paused) or 1 (active), other values may return an error
+	Status int `url:"status,omitempty"`
 }
 
 type PSPResp struct {
-    Stat string `json:"stat"`
-    PSP  struct {
-        ID int `json:"id"`
-    } `json:"psp"`
+	Stat string `json:"stat"`
+	PSP  struct {
+		ID int `json:"id"`
+	} `json:"psp"`
 }
 
 type EditPSPParams struct {
-    ID int `url:"id"`
-    NewPSPParams
+	ID int `url:"id"`
+	NewPSPParams
 }
 
 type DeletePSPParams struct {
-    ID int `url:"id"`
+	ID int `url:"id"`
 }
 
 // GetPSPs Get https://uptimerobot.com/#getPSPsWrap
 func (ps *PSPService) GetPSPs(params GetPSPsParams) (*PSPsResp, error) {
-    obj := &PSPsResp{}
-    err := ps.apiClient.request(http.MethodPost, "getPSPs", params, obj)
-    return obj, err
+	obj := &PSPsResp{}
+	err := ps.apiClient.request(http.MethodPost, "getPSPs", params, obj)
+	return obj, err
 }
 
 // NewPSP Get https://uptimerobot.com/#newPSPWrap
 func (ps *PSPService) NewPSP(params NewPSPParams) (*PSPResp, error) {
-    obj := &PSPResp{}
-    err := ps.apiClient.request(http.MethodPost, "newPSP", params, &obj)
-    return obj, err
+	obj := &PSPResp{}
+	err := ps.apiClient.request(http.MethodPost, "newPSP", params, &obj)
+	return obj, err
 }
 
 // EditPSP Get https://uptimerobot.com/#editPSPWrap
 func (ps *PSPService) EditPSP(params EditPSPParams) (*PSPResp, error) {
-    obj := &PSPResp{}
-    err := ps.apiClient.request(http.MethodPost, "editPSP", params, &obj)
-    return obj, err
+	obj := &PSPResp{}
+	err := ps.apiClient.request(http.MethodPost, "editPSP", params, &obj)
+	return obj, err
 }
 
 // DeletePSP Get https://uptimerobot.com/#deletePSPWrap
 func (ps *PSPService) DeletePSP(id int) (*PSPResp, error) {
-    obj := &PSPResp{}
-    params := DeletePSPParams{ID: id}
-    err := ps.apiClient.request(http.MethodPost, "deletePSP", params, obj)
-    return obj, err
+	obj := &PSPResp{}
+	params := DeletePSPParams{ID: id}
+	err := ps.apiClient.request(http.MethodPost, "deletePSP", params, obj)
+	return obj, err
 }
